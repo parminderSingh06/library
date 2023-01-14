610d0f6f40fa206d677656fbe32415e4126465c1
Parminder Singh <parmindersingh1086@gmail.com>
2023-01-13 23:32:02 -0800

    Buttons now have full functionality

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-let library = [];
+const library = [];
 let formOpen = false;
+let editIndex = 0;
+
 const grid = document.querySelector('.library');
 const newbtn = document.querySelector('#new');
 const formDiv = document.querySelector('#formDiv');
 const form = document.querySelector('#bkForm');
 const overlay = document.querySelector('#overlay');
 const addBookBtn = document.querySelector('#bkForm');
+const errorPG = document.querySelector('#errorPG');
+const errorPR = document.querySelector('#errorPR');
+const editSubmit = document.querySelector('#editSubmit');
+const editPR = document.querySelector('#editPR');
+const editDiv = document.querySelector('.editDiv');
 
 newbtn.addEventListener('click', () => {
   openForm();
@@ -15,6 +22,12 @@ overlay.addEventListener('click', () => {
 });
 addBookBtn.addEventListener('submit', (e) => {
   e.preventDefault();
+  let pr = document.forms['bkForm']['pagesRead'].value;
+  let pg = document.forms['bkForm']['bookPages'].value;
+  if (pg < pr) {
+    errorPR.innerHTML = 'Pages read cannot be greater than book pages!';
+    return;
+  }
   createBook();
 });
 
@@ -24,11 +37,6 @@ function Book(title, author, pages, pagesRead) {
   this.pages = pages;
   this.pagesRead = pagesRead;
 }
-
-Book.prototype.info = function () {
-  let info = `${this.title}\nBy ${this.author}\nPages Read:${this.pagesRead}\nBook Pages:${this.pages}`;
-  return info;
-};
 
 function displayLib() {
   for (let i = 0; i < library.length; i++) {
@@ -55,6 +63,8 @@ function formatBookDisplay(book, card) {
   let btnDiv = document.createElement('div');
   let pgValue = document.createElement('button');
   let rmBook = document.createElement('button');
+  rmBook.setAttribute('onclick', 'deleteBook(this)');
+  pgValue.setAttribute('onclick', 'displayEdit(this)');
   btnDiv.classList.toggle('bookbtn');
   pgValue.innerHTML = 'Read';
   rmBook.innerHTML = 'Remove';
@@ -72,8 +82,10 @@ function openForm() {
 function closeForm() {
   formDiv.setAttribute('style', 'display: none');
   overlay.setAttribute('style', 'display:none');
+  editDiv.setAttribute('style', 'display: none');
   formOpen = false;
   form.reset();
+  editPR.value = '';
 }
 
 function createBook() {
@@ -93,4 +105,48 @@ function deleteOld() {
   for (let i = 0; i < allCards.length; i++) {
     allCards[i].remove();
   }
+}
+
+function deleteBook(a) {
+  let bookEle = a.parentNode.parentNode;
+  let eleTitle = bookEle.firstChild.innerHTML;
+  for (let i = 0; i < library.length; i++) {
+    let temp = `TITLE: ${library[i].title}`;
+    if (temp === eleTitle) {
+      library.splice(i, 1);
+    }
+  }
+  deleteOld();
+  displayLib();
+}
+
+function displayEdit(a) {
+  editDiv.setAttribute('style', 'display: flex');
+  overlay.setAttribute('style', 'display:flex');
+  let bookEle = a.parentNode.parentNode;
+  let eleTitle = bookEle.firstChild.innerHTML;
+  for (let i = 0; i < library.length; i++) {
+    let temp = `TITLE: ${library[i].title}`;
+    if (temp === eleTitle) {
+      editIndex = i;
+      editPR.setAttribute('placeholder', `${library[i].pagesRead}`);
+    }
+  }
+}
+
+function editPages() {
+  if (editPR.value <= library[editIndex].pagesRead) {
+    errorPG.innerHTML = 'New pages read cant be less than previous';
+    editPR.value = '';
+    return;
+  }
+  if (editPR.value > library[editIndex].pages) {
+    errorPG.innerHTML = 'Pages read cant be higher than pages';
+    editPR.value = '';
+    return;
+  }
+  library[editIndex].pagesRead = editPR.value;
+  deleteOld();
+  displayLib();
+  closeForm();
 }
